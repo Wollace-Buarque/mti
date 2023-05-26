391bc52a7a0ac3171e086519e65a1e2b4cc5b09a
Wollace Buarque <wollacebuarque@outlook.com>
2023-05-26 12:56:22 -0300

    Update Footer.tsx

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -6,14 +6,14 @@ export default function Footer() {
       <address>
         <a
           className="flex items-center gap-2 not-italic text-gray-300 hover:text-gray-200 transition-colors group"
-          href="tel:+5581996504241"
+          href="tel:+558190000-0000"
           title="Clique para fazer uma ligação">
             
           <Phone
             className="opacity-0 group-hover:opacity-100 transition-opacity"
             color="#EBA417"/>
 
-          Suporte: (81) 99650-4241
+          Suporte: (81) 90000-0000
         </a>
       </address>
     </footer>
