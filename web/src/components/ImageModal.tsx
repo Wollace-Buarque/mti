d57e6ab573b59fba4382ad694819cc18f8a59247
Wollace Buarque <wollacebuarque@outlook.com>
2024-08-10 21:26:30 -0300

    feat: improve images with modal design

--- web/src/components/ImageModal.tsx
+++ web/src/components/ImageModal.tsx
@@ -1,46 +1,34 @@
-import { X } from "phosphor-react";
+import { ComponentProps } from "react";
+
+import { X } from "@phosphor-icons/react";
 
 import * as Dialog from "@radix-ui/react-dialog";
 
-interface ImageModalProps {
-  src: string | undefined;
-  alt?: string;
-  title?: string;
-  className?: string;
-  draggable?: boolean;
-}
+interface ImageModalProps extends ComponentProps<"img"> {}
 
-export default function ImageModal(props: ImageModalProps) {
-
+export function ImageModal({ className, ...rest }: ImageModalProps) {
   return (
     <Dialog.Root>
       <Dialog.Trigger asChild>
-        <img
-          src={props.src}
-          alt={props.alt}
-          title={props.title}
-          className={`cursor-pointer ${props.className}`}
-          draggable={props.draggable} />
+        <img {...rest} className={`cursor-pointer ${className}`} />
       </Dialog.Trigger>
 
       <Dialog.Portal>
-        <Dialog.Overlay className="bg-black/60 inset-0 fixed" >
+        <Dialog.Overlay className="bg-black/60 inset-0 fixed">
           <Dialog.Content className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[95vw] sm:w-[80vw]">
-
             <img
+              {...rest}
               className="size-full object-cover rounded shadow-image"
-              src={props.src}
-              alt={props.alt}
-              title={props.title}
-              draggable={false} />
+              draggable={false}
+            />
 
             <Dialog.Close title="Fechar imagem">
               <X
                 className="absolute top-2 right-2 hover:animate-spin-one-time"
                 color="#EBA417"
-                size={30} />
+                size={30}
+              />
             </Dialog.Close>
-
           </Dialog.Content>
         </Dialog.Overlay>
       </Dialog.Portal>
