dde0eea8abac9583537c5ac81841b70d9350ef1a
Wollace Buarque <wollacebuarque@outlook.com>
2024-08-10 23:15:08 -0300

    feat: improve crop image design

--- web/src/components/CropImage/CropImageModal.tsx
+++ web/src/components/CropImage/CropImageModal.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
-import Cropper, { Area, Point, Size } from "react-easy-crop";
-
 import * as Dialog from "@radix-ui/react-dialog";
+import { useState } from "react";
+import Cropper, { Area, Point } from "react-easy-crop";
 
 import Button from "../Button";
 import getCroppedImg from "./crop-image";
+import { Scissors } from "@phosphor-icons/react";
 
 interface CropImageProps {
   image: string;
@@ -14,36 +14,42 @@ interface CropImageProps {
 }
 
 export function CropImageModal(props: CropImageProps) {
-  const [croppedAreaPixels, setCroppedAreaPixels] = useState<Area>({ x: 0, y: 0, width: 0, height: 0 });
-  const [crop, setCrop] = useState<Point>({ x: 0, y: 0 })
-  const [zoom, setZoom] = useState(1)
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState<Area>({
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+  });
+  const [crop, setCrop] = useState<Point>({ x: 0, y: 0 });
+  const [zoom, setZoom] = useState(1);
 
   const onCropComplete = async (_: Area, croppedAreaPixels: Area) => {
-    setCroppedAreaPixels(croppedAreaPixels)
-  }
+    setCroppedAreaPixels(croppedAreaPixels);
+  };
 
   const onCropFinish = async () => {
     props.setOpen(false);
 
     const croppedImage = await getCroppedImg(props.image, croppedAreaPixels);
 
-    props.onCropComplete(croppedImage)
-  }
+    props.onCropComplete(croppedImage);
+  };
 
   return (
     <Dialog.Root open={props.open} onOpenChange={props.setOpen}>
       <Dialog.Portal>
         <Dialog.Overlay className="bg-black/60 inset-0 fixed">
-
-          <Dialog.Content className="bg-[#181818] fixed py-8 px-4 text-white top-1/2 left-1/2 -translate-x-1/2 
-    -translate-y-1/2 rounded-lg w-[95%] sm:w-[650px] shadow-lg shadow-black/25">
-
-            <Dialog.Title className="text-3xl text-center font-inter font-black mb-6">
-              Recortar imagem
+          <Dialog.Content
+            className="bg-[#181818] fixed py-8 px-4 text-white top-1/2 left-1/2 -translate-x-1/2 
+    -translate-y-1/2 rounded-lg w-[95%] sm:w-[650px] shadow-lg shadow-black/25"
+          >
+            <Dialog.Title className="text-3xl text-center font-inter font-black mb-6 flex items-center justify-center gap-2">
+              Recortar imagem <Scissors weight="bold"/>
             </Dialog.Title>
 
-            <div className="relative w-full h-96 flex justify-center items-center">
-
+            <Dialog.Description
+              className="relative w-full h-96 flex justify-center items-center"
+            >
               <Cropper
                 image={props.image}
                 cropShape="round"
@@ -56,16 +62,18 @@ export function CropImageModal(props: CropImageProps) {
                 onCropChange={setCrop}
                 onCropComplete={onCropComplete}
                 onZoomChange={setZoom}
-                restrictPosition />
+                restrictPosition
+              />
+              <div
+                className="w-full h-96 bg-center bg-no-repeat bg-cover blur-2xl -z-10"
+                style={{ backgroundImage: `url(${props.image})` }}
+              />
+            </Dialog.Description>
 
-            </div>
-
-            <Button
-              onClick={onCropFinish}
-              title="Recortar" />
+            <Button onClick={onCropFinish} title="Recortar" />
           </Dialog.Content>
         </Dialog.Overlay>
       </Dialog.Portal>
     </Dialog.Root>
-  )
+  );
 }
