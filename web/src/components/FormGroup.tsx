599458f05c323832fa4ec3cb7c90f447fd3ad25c
Wollace Buarque <wollacebuarque@outlook.com>
2024-08-10 22:31:18 -0300

    improve textarea padding

--- web/src/components/FormGroup.tsx
+++ web/src/components/FormGroup.tsx
@@ -10,27 +10,30 @@ export default function FormGroup(props: FormGroupProps) {
   const { name, title, placeholder, type, textarea, ...rest } = props;
 
   return (
-    <div {...rest} className={`flex flex-col uppercase text-sm font-semibold ${props.className}`}>
+    <div
+      {...rest}
+      className={`flex flex-col uppercase text-sm font-semibold ${props.className}`}
+    >
+      <label htmlFor={name}>{title}</label>
 
-      <label htmlFor={name}>
-        {title}
-      </label>
-
-      {textarea
-        ? <textarea
+      {textarea ? (
+        <textarea
           className="bg-[#252222] p-2 rounded-sm text-text font-normal placeholder-text placeholder:text-sm focus:outline-1 focus:outline-button-base focus:outline resize-none"
           rows={6}
           id={name}
           name={name}
-          placeholder={placeholder} />
-
-        : <input
+          placeholder={placeholder}
+          style={{ paddingBlockEnd: "0.5rem" }}
+        />
+      ) : (
+        <input
           className="bg-[#252222] p-2 rounded-sm text-text font-normal placeholder-text placeholder:text-sm focus:outline-1 focus:outline-button-base focus:outline"
           id={name}
           name={name}
           type={type ?? "text"}
-          placeholder={placeholder} />}
-
+          placeholder={placeholder}
+        />
+      )}
     </div>
-  )
+  );
 }
