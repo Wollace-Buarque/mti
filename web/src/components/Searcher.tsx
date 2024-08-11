a41655083383e6569ff043e2b32b0c590944686c
Wollace Buarque <wollacebuarque@outlook.com>
2024-08-10 21:09:48 -0300

    refactor: update icon library and ident code

--- web/src/components/Searcher.tsx
+++ web/src/components/Searcher.tsx
@@ -1,4 +1,4 @@
-import { MagnifyingGlass } from "phosphor-react";
+import { MagnifyingGlass } from "@phosphor-icons/react";
 import { useEffect, useState } from "react";
 
 interface SearcherProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -15,22 +15,21 @@ export default function Searcher(props: SearcherProps) {
   }, [value]);
 
   return (
-    <div
-      className={`flex items-center ${className}`} {...rest}>
-
+    <div className={`flex items-center ${className}`} {...rest}>
       <div className="flex items-center group w-full">
-
         <input
           className="p-2 pl-5 w-full bg-transparent border border-line outline-none rounded-l-full group-hover:border-button-base focus:border-button-base"
           value={value}
           placeholder="Pesquisar"
-          onChange={event => setValue(event.target.value)} />
+          onChange={(event) => setValue(event.target.value)}
+        />
       </div>
 
       <MagnifyingGlass
         className="box-content py-2 px-3 bg-[#222222] border border-l-0 border-line rounded-r-full cursor-pointer"
         onClick={props.onSearchClicker}
-        size={24} />
+        size={24}
+      />
     </div>
-  )
+  );
 }
