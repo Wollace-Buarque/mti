022a6fc9ab3f2e571b87a9f47c641e4f1b1db3bf
Wollace Buarque <wollacebuarque@outlook.com>
2024-08-11 00:45:43 -0300

    feat: improve design, add spinners and update export

--- web/src/components/Account/Report.tsx
+++ web/src/components/Account/Report.tsx
@@ -1,6 +1,7 @@
 import { UploadSimple } from "@phosphor-icons/react";
 import { useContext, useEffect, useState } from "react";
 import { useDropzone } from "react-dropzone";
+import { toast } from "sonner";
 
 import { AuthenticateContext, User } from "../../context/AuthenticateContext";
 import { server } from "../../services/server";
@@ -12,13 +13,16 @@ interface ReportProps {
   user: User;
 }
 
-export default function Report(props: ReportProps) {
+export function Report({ user }: ReportProps) {
   const { setUser } = useContext(AuthenticateContext);
+
   const [file, setFile] = useState<File | null>(null);
+  const [isUploading, setIsUploading] = useState(false);
 
   const { getRootProps, getInputProps, acceptedFiles } = useDropzone({
     accept: {
-      "image/*": [".png", /*".gif",*/ ".jpeg", ".jpg"],
+      "image/png": [".png"],
+      "image/jpeg": [".jpeg", ".jpg"],
     },
     maxFiles: 1,
   });
@@ -27,12 +31,10 @@ export default function Report(props: ReportProps) {
     setFile(acceptedFiles[0]);
   }, [acceptedFiles]);
 
-  if (!props.user) {
-    return null;
-  }
+  if (!user) return null;
 
   async function uploadReport() {
-    if (!props.user) {
+    if (!user) {
       showToast({
         message:
           "Não foi possível validar sua conta, tente fazer login novamente.",
@@ -50,51 +52,59 @@ export default function Report(props: ReportProps) {
     }
 
     const uploadData = new FormData();
-    uploadData.append("email", props.user.email);
+    uploadData.append("email", user.email);
     uploadData.append("type", "report");
     uploadData.append("file", file);
 
-    const { data } = await server.post("/report", uploadData);
+    setIsUploading(true);
 
-    if (data.message !== "Report changed.") {
-      setFile(null);
-      showToast({
-        message: "Ocorreu um erro ao enviar o laudo médico.",
-        type: "warning",
-      });
-      return;
-    }
+    toast.promise(server.post("/report", uploadData), {
+      loading: "Seu relatório está sendo encaminhado... 🚚",
+      success: (response) => {
+        setUser({
+          ...user,
+          report: response.data.report,
+        });
 
-    setUser({
-      ...props.user,
-      report: data.report,
+        return "Seu relatório foi atualizado.";
+      },
+      error: "Algo deu errado o seu relatório.",
+      finally: () => {
+        acceptedFiles.pop();
+        setIsUploading(false);
+        setFile(null);
+      },
     });
-
-    showToast({ message: "Laudo médico atualizado." });
-    setFile(null);
   }
 
   return (
     <div className="mt-6">
       <input {...getInputProps()} />
 
-      <div className="flex flex-col sm:flex-row items-center justify-between gap-3">
-        <h2 className="flex items-center gap-2 text-title text-3xl">
-          {props.user.report ? "Seu" : "Adicionar"} laudo médico
+      <div className="flex flex-col sm:flex-row items-center sm:items-end justify-between gap-3">
+        <h2 className="flex items-center gap-2 text-title text-3xl group">
+          {user.report ? "Seu" : "Adicionar"} laudo médico
           <em {...getRootProps()}>
-            <UploadSimple color="#EBA417" className="cursor-pointer" />
+            <span className="sr-only">Adicionar imagem de relatório</span>
+
+            <UploadSimple
+              color="#EBA417"
+              className="cursor-pointer group-hover:scale-110 transition-transform"
+            />
           </em>
         </h2>
 
         <Button
-          className={`${file ? "block" : "hidden"} h-14 sm:w-28 mt-0`}
           onClick={uploadReport}
-          title="Enviar"
-        />
+          isLoading={isUploading}
+          className={`${file ? "block" : "hidden"} sm:w-28 flex items-center justify-center`}
+        >
+          {user.report ? "Atualizar" : "Enviar"}
+        </Button>
       </div>
 
       <ImageModal
-        src={file ? URL.createObjectURL(file) : props.user.report?.reportUrl}
+        src={file ? URL.createObjectURL(file) : user.report?.reportUrl}
         className="shadow-elevation rounded mt-4"
         draggable={false}
       />
