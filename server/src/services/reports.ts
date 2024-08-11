67fd2e6c9665111dc0259859889225acda619a09
Wollace Buarque <wollacebuarque@outlook.com>
2024-08-11 01:41:06 -0300

    feat: improve reports upload route

--- server/src/services/reports.ts
+++ server/src/services/reports.ts
@@ -1,30 +1,76 @@
 import express from "express";
 
 import { prisma } from "../server";
+import fs from "fs";
+import path from "path";
 
 const REPORTS_BASE_URL = "http://localhost:3000/reports";
 
-async function changeReport(request: express.Request, response: express.Response) {
-  const { email } = request.body;
+async function changeReport(
+  request: express.Request,
+  response: express.Response,
+) {
+  const bearer = request.headers.authorization;
+  const token = bearer?.split(" ")[1];
 
-  const user = await prisma.user.update({
+  if (!token) {
+    return response.status(400).json({
+      message: "Invalid credentials.",
+    });
+  }
+
+  const user = await prisma.user.findUnique({
     where: {
-      email,
+      token,
     },
-    data: {
-      report: {
-        upsert: {
-          create: {
-            reportUrl: request.file?.filename ?? "",
-          },
-          update: {
-            reportUrl: request.file?.filename ?? "",
-            updatedAt: new Date(),
-          }
-        }
-      },
-    }
+    select: {
+      id: true,
+      report: true,
+    },
   });
+
+  if (!user) {
+    return response.status(404).json({
+      message: "Account not exists.",
+    });
+  }
+
+  const oldReport = user.report?.reportUrl;
+  const fileName = request.file?.filename;
+
+  if (!fileName) {
+    return response.status(400).json({
+      message: "File sent not found.",
+    });
+  }
+
+  try {
+    await prisma.report.upsert({
+      where: {
+        userId: user.id,
+      },
+      update: {
+        reportUrl: fileName,
+      },
+      create: {
+        reportUrl: fileName,
+        user: {
+          connect: {
+            id: user.id,
+          },
+        },
+      },
+    });
+
+    if (oldReport)
+      fs.unlinkSync(
+        path.resolve(__dirname, "..", "..", "database", "reports", oldReport),
+      );
+  } catch (error) {
+    return response.status(400).json({
+      message: "Error while tried to change report!",
+    });
+  }
 
   const report = await prisma.report.findUnique({
     where: {
@@ -33,25 +79,23 @@ async function changeReport(request: express.Request, response: express.Response
   });
 
   if (!user) {
-    return response.status(200).json({
-      message: "Account not exists."
+    return response.status(404).json({
+      message: "Account not exists.",
     });
   }
 
   if (!report) {
-    return response.status(200).json({
-      message: "Could not change report."
+    return response.status(400).json({
+      message: "Could not change report.",
     });
   }
 
   report.reportUrl = `${REPORTS_BASE_URL}/${report?.reportUrl}`;
 
-  response.status(202).json({
+  response.status(200).json({
     message: "Report changed.",
-    report
+    report,
   });
 }
 
-export {
-  changeReport
-}
+export { changeReport };
